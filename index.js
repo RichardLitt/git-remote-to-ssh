bc8a90e0d12f69b7d6b23f2a5ebc9f7679563925
Richard Littauer <richard.littauer@gmail.com>
2016-09-23 14:26:24 -0400

    Remove arrow function
    If anyone sees this, why didnt this work X|

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 #! /usr/local/bin/node
 const shell = require("shelljs")
 const fs = require('fs')
-const fix = fs.readFile('fix_github_https_repo.sh', 'utf8', (err, file) => shell.exec(file))
+const fix = fs.readFile('fix_github_https_repo.sh', 'utf8', function (err, file) {
+  shell.exec(file)
+})
