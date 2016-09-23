6a4dc4816d07eef1847ed12fdbb50333c06768d0
Richard Littauer <richard.littauer@gmail.com>
2016-09-23 14:42:37 -0400

    Remove all the js

--- index.js
+++
@@ -1,6 +0,0 @@
-#! /usr/local/bin/node
-const shell = require("shelljs")
-const fs = require('fs')
-const fix = fs.readFile('fix_github_https_repo.sh', 'utf8', function (err, file) {
-  shell.exec(file)
-})
